2ba4112e82891369e7962d7972e4d5c0a2f74d24
bxy0516 <601789175@qq.com>
2019-07-15 15:58:44 +0800

    添加建造者模式和深克隆

---
+++ src/main/java/org/ayahiro/practice/design_patterns/behavioral/builder/Builder.java
@@ -0,0 +1,100 @@
+package org.ayahiro.practice.design_patterns.behavioral.builder;
+
+/**
+ * @Author ayahiro
+ * @Description:
+ * @Create: 2019/7/15
+ */
+public class Builder {
+    public static void main(String[] args) {
+        CharacterPanel characterPanel = new CharacterPanel();
+        Character swordsman = characterPanel.createCharacter(new SwordsmanBuilder());
+        Character gunner = characterPanel.createCharacter(new GunnerBuilder());
+        swordsman.show();
+        gunner.show();
+    }
+}
+
+//Product
+class Character {
+    String weapon;
+    String guard;
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public String getGuard() {
+        return guard;
+    }
+
+    public void setGuard(String guard) {
+        this.guard = guard;
+    }
+
+    public void show() {
+        System.out.println("这个角色的武器是：" + weapon + "，防具精通：" + guard);
+    }
+}
+
+//Builder
+interface CharacterBuilder {
+    void buildWeapon();
+
+    void buildGuard();
+
+    Character createCharacter();
+}
+
+//ConcreteBuilder1
+class SwordsmanBuilder implements CharacterBuilder {
+    Character character = new Character();
+
+    @Override
+    public void buildWeapon() {
+        character.setWeapon("残破的剑");
+    }
+
+    @Override
+    public void buildGuard() {
+        character.setGuard("重甲");
+    }
+
+    @Override
+    public Character createCharacter() {
+        return character;
+    }
+}
+
+//ConcreteBuilder2
+class GunnerBuilder implements CharacterBuilder {
+    Character character = new Character();
+
+    @Override
+    public void buildWeapon() {
+        character.setWeapon("没子弹的枪");
+    }
+
+    @Override
+    public void buildGuard() {
+        character.setGuard("皮甲");
+    }
+
+    @Override
+    public Character createCharacter() {
+        return character;
+    }
+}
+
+//Director
+class CharacterPanel {
+    public Character createCharacter(CharacterBuilder characterBuilder) {
+        characterBuilder.buildWeapon();
+        characterBuilder.buildGuard();
+        return characterBuilder.createCharacter();
+    }
+}
--- src/main/java/org/ayahiro/practice/design_patterns/behavioral/prototype/Prototype.java
+++ src/main/java/org/ayahiro/practice/design_patterns/behavioral/prototype/Prototype.java
@@ -6,4 +6,76 @@ package org.ayahiro.practice.design_patterns.behavioral.prototype;
  * @Create: 2019/7/15
  */
 public class Prototype {
+    public static void main(String[] args) {
+        Resume resume1 = new Resume("bxy");
+        resume1.setPersonalInfo("male", "21");
+        resume1.setWorkExperience("2017-2021", "NUIST");
+
+        System.out.println(resume1.toString());
+        try {
+            Resume resume2 = (Resume) resume1.clone();
+            System.out.println(resume1.workExperience == resume2.workExperience);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Resume implements Cloneable {
+    String name;
+    String sex;
+    String age;
+    WorkExperience workExperience;
+
+    public Resume(String name) {
+        this.name = name;
+        workExperience = new WorkExperience();
+    }
+
+    public void setPersonalInfo(String sex, String age) {
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public void setWorkExperience(String workDate, String company) {
+        workExperience.workDate = workDate;
+        workExperience.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "Resume{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age='" + age + '\'' +
+                ", workExperience=" + workExperience +
+                '}';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Resume resume = (Resume) super.clone();
+        if (workExperience != null) {
+            resume.workExperience = (WorkExperience) workExperience.clone();
+        }
+        return resume;
+    }
+}
+
+class WorkExperience implements Cloneable {
+    String workDate;
+    String company;
+
+    @Override
+    public String toString() {
+        return "WorkExperience{" +
+                "workDate='" + workDate + '\'' +
+                ", company='" + company + '\'' +
+                '}';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
